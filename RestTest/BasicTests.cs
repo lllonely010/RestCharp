e5a6a2833a0a27a0d37ef63a0d3f0ea5f00b9336
Jessie Li - Dilato IT <dit_j.li@tjip.com>
2019-11-04 15:48:21 +0100

    upload simeple project

---
+++ RestTest/BasicTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using RestSharp;
+using NUnit.Framework;
+
+namespace RestTest
+{
+    [TestFixture]
+    public class BasicTests
+    {
+        [Test]
+        public void StatusCodeTest()
+        {
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            RestRequest request = new RestRequest("nl/3825", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void ContentTypeTest()
+        {
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            RestRequest request = new RestRequest("nl/3825", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            Assert.That(response.ContentType, Is.EqualTo("application/json"));
+        }
+      
+    }
+}
---
+++ RestTest/DataDrivenTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using RestSharp;
+using NUnit.Framework;
+
+namespace RestTest
+{
+    [TestFixture]
+    public class DataDrivenTest
+    {
+        [TestCase("nl", "2611", HttpStatusCode.OK, TestName = "Check the status code for NL2611")]
+        [TestCase("nl","1234",HttpStatusCode.NotFound,TestName ="Check for status code for NL1000")]
+        public void StatusCodeTest(string countryCode, string zipCode, HttpStatusCode expectedCode)
+        {
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            RestRequest request = new RestRequest($"{countryCode}/{zipCode}", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(expectedCode));
+        }
+
+
+      
+    }
+}
